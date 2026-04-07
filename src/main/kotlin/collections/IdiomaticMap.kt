d63e7d4e06aac7ec0b128cc6ab6e64a42649264e
Eveliina Vesala <eveliina.ves@gmail.com>
2026-04-07 21:58:22 +0300

    chore: remove Idiomatic* collection demos and tests moved to demo source set

--- src/main/kotlin/collections/IdiomaticMap.kt
+++
@@ -1,7 +0,0 @@
-package collections
-
-/**
- * A data class containing a map of strings to integers.
- * This is useful for storing properties or attributes.
- */
-data class UserWithScores(val id: String, val scores: Map<String, Int>)
