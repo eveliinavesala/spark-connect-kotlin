10bcce5a72afd514c91109e2584918ae836f2703
Eveliina Vesala <eveliina.ves@gmail.com>
2026-04-07 21:58:37 +0300

    chore: remove KotlinEncoder superseded by reflection engine

--- src/main/kotlin/encoder/kotlin/KotlinEncoder.kt
+++
@@ -1,4 +0,0 @@
-package encoder.kotlin// This file has been removed to clean up compilation errors.
-// The conceptual code it contained will be re-evaluated and implemented correctly
-// in a later phase of the project.
-// Our current focus is on the pragmatic DataFrame-based approach.
